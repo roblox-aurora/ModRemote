3b3ac72ff8f517c75becda433a6290062823d93e
Jonathan Holmes <Vorlias@users.noreply.github.com>
2017-04-21 11:46:54 +1200

    Added package.lua for RPM

---
+++ Package.lua
@@ -0,0 +1,22 @@
+return {
+	Name = "ModRemote",
+	Version = "4.0",
+	Description = "Easy ROBLOX Networking",
+	Author = {
+		Name = "Vorlias",
+		UserId = 4308133
+	},
+	License = "MIT",
+	Main = "ModRemote",
+	Ignore = {
+    "API",
+    "License",
+    "README",
+	},
+	Platform = {
+		Server = true;
+    Client = true
+	},
+	Dependencies = {
+	}
+}
